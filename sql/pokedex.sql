76607779459020c39ec222984a6c0c834788ced5
pablolickey <60665645+pablolickey@users.noreply.github.com>
2023-09-21 23:21:20 -0300

    sql

--- sql/pokedex.sql
+++ sql/pokedex.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 20-09-2023 a las 21:34:57
+-- Tiempo de generación: 22-09-2023 a las 02:20:49
 -- Versión del servidor: 5.7.31
 -- Versión de PHP: 7.3.21
 
@@ -42,15 +42,17 @@ CREATE TABLE IF NOT EXISTS `pokemon` (
   `img` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `identificador` (`identificador`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `pokemon`
 --
 
 INSERT INTO `pokemon` (`id`, `identificador`, `nombre`, `tipo`, `descripcion`, `vida`, `defensa`, `peso`, `velocidad`, `ataque`, `img`) VALUES
-(1, 1, 'Pikachu', 4, 'Pikachu almacena una gran cantidad de electricidad en sus mejillas. Estas parecen cargarse elÃ©ctricamente durante la noche mientras duerme. Las mejillas de Pikachu tambiÃ©n pueden ser recargadas mediante una descarga elÃ©ctrica, como se ha podido observar en algunos episodios del anime. A veces suelta unas pequeÃ±as descargas cuando se acaba de despertar. Las mejillas son las que generan electricidad, pero esta es conducida y descargada por la punta de su cola produciendo descargas elÃ©ctricas, que aumentan de poder dependiendo del estado de Ã¡nimo de Pikachu. Muchas veces, en las tormentas se juntan y absorben electricidad de los relÃ¡mpagos.', 65, 65, 65, 65, 65, '1.gif'),
-(6, 2, 'bulbasaur', 3, 'Bulbasaur es un PokÃ©mon cuadrÃºpedo de color verde, posee manchas de una tonalidad mÃ¡s oscura del mismo color con distintas formas geomÃ©tricas. Su cabeza representa cerca de un tercio de su cuerpo. En su frente se ubican tres manchas que pueden cambiar de posiciÃ³n, forma o lugar dependiendo del ejemplar. Tiene como orejas muÃ±ones pequeÃ±os y puntiagudos. Sus ojos son grandes y de color rojo. Sus patas son cortas y posee tres garras en cada una. Este PokÃ©mon tiene plantado un bulbo en el lomo desde que nace, esta semilla crece y se desarrolla a lo largo del ciclo de vida de Bulbasaur a medida que suceden sus evoluciones. El bulbo absorbe y almacena la energÃ­a solar que Bulbasaur necesita para hacer florecer el bulbo de su lomo y evolucionar en Ivysaur. Dicen que cuanta mÃ¡s luz consuma la semilla, mÃ¡s rÃ¡pido crecerÃ¡ y brotarÃ¡, por lo que es muy comÃºn ver a este PokÃ©mon tumbado echÃ¡ndose una siesta en lugares donde los rayos del sol lleguen a plenitud. Por otro lado, gracias a los nutrientes que el bulbo almacena, puede pasar varios dÃ­as sin comer. Su cuerpo segÃºn a palabras de Ken Sugimori y Junichi Masuda en una entrevista, estÃ¡ basado en un anfibio (sapo o rana), al igual que toda su lÃ­nea evolutiva.3\r\n\r\nEl bulbo de Bulbasaur le ayuda a defenderse de los enemigos, y desde Ã©l puede disparar ataques tales como rayo solar y drenadoras entre otros movimientos.\r\n\r\nNo es muy raro encontrarlo en jardines y zonas cercanas a fuentes de agua. Se los puede atraer con el aroma de las flores. SegÃºn el anime, una vez al aÃ±o, cuando estos PokÃ©mon estÃ¡n listos para evolucionar suelen reunirse en grandes cantidades en un JardÃ­n Misterioso mientras hacen un ritual a la luz de la luna junto a un gran Venusaur.\r\nEste contenido proviene de wikidex.net, y debe darse atribuciÃ³n a sus autores, tal como especifica la licencia.\r\nSe prohÃ­be su uso a PlagioDex (el wiki de FANDOOM), por copiar reiteradamente sin dar atribuciÃ³n', 50, 25, 15, 15, 25, '6.png');
+(7, 1, 'Bulbasaur', 3, 'A Bulbasaur es facil de verle echandose una siesta al sol. La semilla que tiene en el lomo va creciendo cada vez mas a medida que absorbe los rayos del sol ', 80, 80, 7, 3, 3, '7.gif'),
+(8, 25, 'Pikachu', 4, 'Cada vez que un Pikachu se encuentra con algo nuevo, le lanza una descarga electrica.Cuando se ve alguna baya chamuscada, es muy probable que sea obra de un Pikachu, ya que a veces no controlan la intensidad de la descarga', 435, 67, 5, 90, 55, '8.gif'),
+(9, 143, 'Snorlax', 4, 'Snorlax es un Pokemon corpulento y dormilón. Se caracteriza por su gran tamaño y su pelaje azul verdoso. Es conocido por su amor por el sueño y su apetito voraz. A menudo, bloquea caminos y carreteras al dormir profundamente en medio de la ruta, lo que lo convierte en un obstáculo molesto para los entrenadores. A pesar de su naturaleza tranquila, cuando se enfurece o es despertado, puede convertirse en un poderoso oponente en batallas. Es un Pokemon Normal de gran resistencia y potencia.', 160, 65, 460, 30, 110, '9.gif'),
+(10, 4, 'Charmender', 2, 'La llama que tiene en la punta de la cola arde segun sus sentimientos. Llamea levemente cuando está alegre y arde vigorosamente cuando esta enfadado', 39, 43, 9, 65, 52, '10.gif');
 
 -- --------------------------------------------------------
 
