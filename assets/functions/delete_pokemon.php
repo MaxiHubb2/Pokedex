fb6ae65881e3ec80d86ddd1492ebcb3ec4075a7d
pablolickey <60665645+pablolickey@users.noreply.github.com>
2023-09-23 08:54:26 -0300

    fix en delete

--- assets/functions/delete_pokemon.php
+++ assets/functions/delete_pokemon.php
@@ -1,10 +1,17 @@
 <?php
     require("db_functions.php");
 
-    if(isset($_POST['identificador'])){
-        $identificador = $_POST['identificador'];
-        ejecutar_query("DELETE FROM pokemon WHERE identificador = $identificador");
-        header("Location: ../../vistas/busqueda.php");
-        exit();
+    try{
+        if(isset($_POST['identificador'])){
+            $identificador = $_POST['identificador'];
+            $img=ejecutar_query("SELECT img FROM pokemon WHERE identificador = $identificador",true)[0]->img;
+            if(ejecutar_query("DELETE FROM pokemon WHERE identificador = $identificador")){
+                unlink("../../uploads/$img");
+                header("Location: ../../vistas/busqueda.php");
+                exit();
+            }else throw new error ("Error al eliminar pokémon de la db");
+        }
+    }catch(Exception $e){
+        throw new error("Error al eliminar pokémon");
     }
 ?>
